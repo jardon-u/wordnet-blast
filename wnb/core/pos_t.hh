4ab1d981e7ccfdf218b78c3db8141d7766995509
Javier G. Sogo <jgsogo@gmail.com>
2015-08-19 09:57:31 +0200

    all strings in one place

--- wnb/core/pos_t.hh
+++ wnb/core/pos_t.hh
@@ -1,11 +1,14 @@
 #ifndef _POS_T_HH
 # define _POS_T_HH
 
+#include <string>
+
 namespace wnb
 {
 
   static const std::size_t POS_ARRAY_SIZE = 6;
-  static const char POS_ARRAY[POS_ARRAY_SIZE] = {'u', 'n', 'v', 'a', 'r', 's'};
+  static const char POS_ARRAY[POS_ARRAY_SIZE] = { 'u', 'n', 'v', 'a', 'r', 's' };
+  static const std::string POS_NAME[POS_ARRAY_SIZE] = { "UNKNOWN", "noun", "verb", "adj", "adv", "adj sat" };
 
   enum pos_t
   	{
@@ -20,43 +23,27 @@ namespace wnb
 
   inline pos_t get_pos_from_name(const std::string& pos)
   {
-    if (pos == "adj")
-      return A;
-    if (pos == "noun")
-      return N;
-    if (pos == "adv")
-      return R;
-    if (pos == "verb")
-      return V;
-    if (pos == "adj sat")
-      return S;
-    return UNKNOWN;
+      for (std::size_t i = 0; i < POS_ARRAY_SIZE; ++i) {
+          if (pos.compare(POS_NAME[i]) == 0) {
+              return static_cast<pos_t>(i);
+          }
+      }
+      return pos_t::UNKNOWN;    
   }
 
   inline std::string get_name_from_pos(const pos_t& pos)
   {
-    switch (pos)
-    {
-    case A: return "adj";
-    case N: return "noun";
-    case R: return "adv";
-    case V: return "verb";
-    case S: return "adj sat";
-    default: return "UNKNOWN";
-    }
+    return POS_NAME[pos];    
   }
 
   inline pos_t get_pos_from_char(const char& c)
   {
-    switch (c)
-    {
-    case 'a': return A;
-    case 'n': return N;
-    case 'r': return R;
-    case 'v': return V;
-    case 's': return S;
-    default: return UNKNOWN;
-    }
+      for (std::size_t i = 0; i < POS_ARRAY_SIZE; ++i) {
+          if (c == POS_ARRAY[i]) {
+              return static_cast<pos_t>(i);
+          }
+      }
+      return pos_t::UNKNOWN;
   }
 
 } // end of namespace wncpp
